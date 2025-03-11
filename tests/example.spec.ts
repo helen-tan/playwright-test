3d5e6059aad97413df424084028a222937b18075
Helen Tan <helentanmz0909@gmail.com>
2025-03-11 11:53:52 +0800

    strict vs loose assertions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,13 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/); // More loose
+  // await expect(page).toHaveTitle("Fast and reliable end -- to -- end testing for modern web apps | Plauywright"); // Strict assertion
+
+  // Note:
+  // Assertion with hardcoded string (strict) vs a regex (more loose)
+  // Strict assertions will increase the cost of maintenance
+  // Minor change to your code will require you to update your test script.
 });
 
 test('get started link', async ({ page }) => {
